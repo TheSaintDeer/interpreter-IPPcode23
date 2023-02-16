9d769a447cd68b64215ed82c954fcd07d14e8349
Golikov Ivan <golikov244@gmail.com>
2023-02-16 09:44:43 +0100

    complite design pattern

--- parse.php
+++ parse.php
@@ -60,6 +60,49 @@ function getLine($writer, $line) {
             case "BREAK":
                 $instruction = new FreeInstr($parts[0]);
                 break;
+
+            case "MOVE":
+            case "INT2CHAR":
+            case "STRLEN":
+            case "TYPE":
+            case "NOT":
+                $instruction = new VarSymbInstr($parts[0], $parts[1], $parts[2]);
+                break;
+
+            case "PUSHS":
+            case "WRITE":
+            case "EXIT":
+            case "DPRINT":
+                $instruction = new SymbInstr($parts[0], $parts[1]);
+                break;
+
+            case "ADD":
+            case "SUB":
+            case "MUL":
+            case "IDIV":
+            case "LT":
+            case "GT":
+            case "EQ":
+            case "AND":
+            case "OR":
+            case "STRI2INT":
+            case "CONCAT":
+            case "GETCHAR":
+            case "SETCHAR":
+                $instruction = new Var2SymbInstr($parts[0], $parts[1], $parts[2], $parts[3]);
+                break;
+
+            case "READ":
+                $instruction = new VarTypeInstr($parts[0], $parts[1], $parts[2]);
+                break;
+
+            case "JUMPIFEQ":
+            case "JUMPIFNEQ":
+                $instruction = new Label2SymbInstr($parts[0], $parts[1], $parts[2], $parts[3]);
+                break;
+
+            default:
+                printOperationCodeError();
         }
 
         $instruction->convertToXML($writer);
@@ -147,6 +190,24 @@ class Writer {
         $this->xml->writeAttribute('type', strtolower($type));
         $this->xml->text($code);
         $this->xml->endElement();
+
+        $this->arg++;
+    }
+
+    public function writeSymb($data) {
+        $dataType = explode("@", $data);
+
+        if (in_array($dataType[0], array("GF", "LF", "TF")))
+            $this->writeArgument("var", $data);
+        elseif (in_array($dataType[0], array("int", "bool", "string", "nil")))
+            $this->writeArgument($dataType[0], $dataType[1]);
+        else
+            printLexicalError();
+    }
+
+    public function writeType($type) {
+        //TODO checking types
+        $this->writeArgument("type", $type);
     }
 
     public function getInstructionOrder() {
@@ -188,15 +249,15 @@ class VarInstr extends InstructionCreator {
 
 class LabelInstr extends InstructionCreator {
 
-    private $inst, $var;
+    private $inst, $label;
 
-    public function __construct(string $inst, string $var) {
+    public function __construct(string $inst, string $label) {
         $this->inst = $inst;
-        $this->var = $var;
+        $this->label = $label;
     }
 
     public function getInstruction(): ArgumentsProducts {
-        return new LabelArg($this->inst, $this->var);
+        return new LabelArg($this->inst, $this->label);
     }
 }
 
@@ -210,6 +271,82 @@ class FreeInstr extends InstructionCreator {
 
     public function getInstruction(): ArgumentsProducts {
         return new FreeArg($this->inst);
+    }
+}
+
+class VarSymbInstr extends InstructionCreator {
+
+    private $inst, $var, $symb;
+
+    public function __construct(string $inst, string $var, string $symb) {
+        $this->inst = $inst;
+        $this->var = $var;
+        $this->symb = $symb;
+    }
+
+    public function getInstruction(): ArgumentsProducts {
+        return new VarSymbArg($this->inst, $this->var, $this->symb);
+    }
+}
+
+class SymbInstr extends InstructionCreator {
+
+    private $inst, $symb;
+
+    public function __construct(string $inst, string $symb) {
+        $this->inst = $inst;
+        $this->symb = $symb;
+    }
+
+    public function getInstruction(): ArgumentsProducts {
+        return new SymbArg($this->inst, $this->symb);
+    }
+}
+
+class Var2SymbInstr extends InstructionCreator {
+
+    private $inst, $var, $symb1, $symb2;
+
+    public function __construct(string $inst, string $var, string $symb1, string $symb2) {
+        $this->inst = $inst;
+        $this->var = $var;
+        $this->symb1 = $symb1;
+        $this->symb2 = $symb2;
+    }
+
+    public function getInstruction(): ArgumentsProducts {
+        return new Var2SymbArg($this->inst, $this->var, $this->symb1, $this->symb2);
+    }
+}
+
+class VarTypeInstr extends InstructionCreator {
+
+    private $inst, $var, $type;
+
+    public function __construct(string $inst, string $var, string $type) {
+        $this->inst = $inst;
+        $this->var = $var;
+        $this->type = $type;
+    }
+
+    public function getInstruction(): ArgumentsProducts {
+        return new VarTypeArg($this->inst, $this->var, $this->type);
+    }
+}
+
+class Label2SymbInstr extends InstructionCreator {
+
+    private $inst, $label, $symb1, $symb2;
+
+    public function __construct(string $inst, string $label, string $symb1, string $symb2) {
+        $this->inst = $inst;
+        $this->label = $label;
+        $this->symb1 = $symb1;
+        $this->symb2 = $symb2;
+    }
+
+    public function getInstruction(): ArgumentsProducts {
+        return new Label2SymbArg($this->inst, $this->label, $this->symb1, $this->symb2);
     }
 }
 
@@ -265,6 +402,98 @@ class FreeArg implements ArgumentsProducts {
     
     public function fillWriter($writer): void {
         $writer->startWriteInstruction($this->inst);
+        $writer->endWriteInstruction();
+    }
+}
+
+class Var2SymbArg implements ArgumentsProducts {
+
+    private $inst, $var, $symb1, $symb2;
+
+    public function __construct(string $inst, string $var, string $symb1, string $symb2) {
+        $this->inst = $inst;
+        $this->var = $var;
+        $this->symb1 = $symb1;
+        $this->symb2 = $symb2;
+    }
+    
+    public function fillWriter($writer): void {
+        $writer->startWriteInstruction($this->inst);
+        $writer->writeArgument("var", $this->var);
+        $writer->writeSymb($this->symb1);
+        $writer->writeSymb($this->symb2);
+        $writer->endWriteInstruction();
+    }
+}
+
+class SymbArg implements ArgumentsProducts {
+
+    private $inst, $symb;
+
+    public function __construct($inst, $symb) {
+        $this->inst = $inst;
+        $this->symb = $symb;
+    }
+    
+    public function fillWriter($writer): void {
+        $writer->startWriteInstruction($this->inst);
+        $writer->writeSymb($this->symb);
+        $writer->endWriteInstruction();
+    }
+}
+
+class VarSymbArg implements ArgumentsProducts {
+
+    private $inst, $var, $symb;
+
+    public function __construct($inst, $var, $symb) {
+        $this->inst = $inst;
+        $this->var = $var;
+        $this->symb = $symb;
+    }
+    
+    public function fillWriter($writer): void {
+        $writer->startWriteInstruction($this->inst);
+        $writer->writeArgument("var", $this->var);
+        $writer->writeSymb($this->symb);
+        $writer->endWriteInstruction();
+    }
+}
+
+class VarTypeArg implements ArgumentsProducts {
+
+    private $inst, $var, $type;
+
+    public function __construct($inst, $var, $type) {
+        $this->inst = $inst;
+        $this->var = $var;
+        $this->type = $type;
+    }
+    
+    public function fillWriter($writer): void {
+        $writer->startWriteInstruction($this->inst);
+        $writer->writeArgument("var", $this->var);
+        $writer->writeType($this->type);
+        $writer->endWriteInstruction();
+    }
+}
+
+class Label2SymbArg implements ArgumentsProducts {
+
+    private $inst, $label, $symb1, $symb2;
+
+    public function __construct(string $inst, string $label, string $symb1, string $symb2) {
+        $this->inst = $inst;
+        $this->label = $label;
+        $this->symb1 = $symb1;
+        $this->symb2 = $symb2;
+    }
+    
+    public function fillWriter($writer): void {
+        $writer->startWriteInstruction($this->inst);
+        $writer->writeArgument("label", $this->label);
+        $writer->writeSymb($this->symb1);
+        $writer->writeSymb($this->symb2);
         $writer->endWriteInstruction();
     }
 }
